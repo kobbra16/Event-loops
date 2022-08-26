aa0e9466555a503123be15f08133f8c52894971d
unknown <dalas10@mail.ru>
2022-08-26 19:51:18 +0300

    'intervals'

---
+++ Event loop.js
@@ -0,0 +1,22 @@
+// Event Loop
+
+
+// const timeout = setTimeout(() => {
+//     console.log("ALTER TIMEOUT")
+// }, 2500)
+// clearTimeout(timeout)
+
+
+
+// const interval = setInterval(() => {
+//     console.log("ALTER TIMEOUT")
+// }, 1000)
+// clearInterval(interval)
+
+
+// const delay = (callback, wait = 1000) => {
+//     setTimeout(callback, wait)
+// }
+// delay(() => {
+//     console.log('After 2 second')
+// }, 2000)
